29ffb244e095212ff2c6931d8900e358fa11b46c
obynonwane <biostechnologyng@gmail.com>
2024-08-03 21:49:33 +0100

    Managing a List of Course Goals (in Our Demo App)

--- App.js
+++ App.js
@@ -6,16 +6,18 @@ export default function App() {
   // declaring useState for state management
   // enteredGoalText - Hold the state 
   // setEnteredGoalText - Function to populate the state
-  const [enteredGoalText, setEnteredGoalText] = useState('')
+  const [enteredGoalText, setEnteredGoalText] = useState('') // just a string 
+  const [courseGoals, setCourseGoals] = useState([]) // array of strings state - set to empty 
 
   // function to manage state
   function goalInputHandler(enteredText){
+    // update the state
     setEnteredGoalText(enteredText);
+    
   }
-
   // perform some actions on the state
   function addGoalHandler(){
-   console.log(enteredGoalText);
+    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText ])
   }
   return (
     <View style={styles.appContainer}>
@@ -24,7 +26,7 @@ export default function App() {
         <Button  title="Add Goal" onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
-        <Text >List of goals...</Text>
+        {courseGoals.map((goal)=><Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
