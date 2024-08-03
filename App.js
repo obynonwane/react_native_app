34e75c99b07ebfba23c3d3865e8df8c442bb15d1
obynonwane <biostechnologyng@gmail.com>
2024-08-03 19:37:11 +0100

    Improving the Layout

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ export default function App() {
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}> 
         <TextInput style={styles.textInput} placeholder="Your course goal"/>
-        <Button title="Add Goal"/>
+        <Button  title="Add Goal"/>
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainer}>
+        <Text >List of goals...</Text>
       </View>
     </View>
   );
@@ -18,19 +18,30 @@ export default function App() {
 const styles = StyleSheet.create({
 
   appContainer:{
-    padding:50,
+    flex: 1,
+    paddingTop:50,
+    paddingHorizontal: 16
   },
 
   inputContainer:{
+    flex: 1,
     flexDirection:'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom:24,
+    borderBottomWidth: 1,
+    borderBlockColor: `#cccccc`
   },
 
   textInput:{
+    alignItems: 'center',
     borderWidth:1,
     borderColor:'#cccccc',
-    width:'80%',
+    width:'70%',
     marginRight:8,
     padding:8 
+  },
+  goalsContainer:{
+    flex:5
   }
 });
